e9f424149fb16b72cc5116abf3fa09a9c8ebc3d6
valeryiay <valeryiayarash@gmail.com>
2023-10-30 00:29:08 +0100

    feat: no pass

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minRange = 1;
+        this.maxRange = 100;
+        this.answer = null;
+    }
 
     setRange(min, max) {
-
+        this.minRange = min;
+        this.maxRange = max;
+        this.answer = null;
     }
 
     guess() {
-
+        return Math.floor((this.minRange + this.maxRange) / 2);
     }
 
     lower() {
-
+        this.maxRange = this.guess() - 1;
     }
 
     greater() {
-
+        this.minRange = this.guess() + 1;
     }
 }
 
